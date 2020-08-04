5c1631fb22aa73c343f5b484af2b44df500ea396
dongyoen-lee <dlehddus1285@naver.com>
2020-08-05 00:01:57 +0900

    대쉬보드 작성

--- routes/board.js
+++ routes/board.js
@@ -1,8 +1,15 @@
 var express = require('express');
+const { log } = require('debug');
 var router = express.Router();
-
-router.get('/',(_, res) =>{
-    res.render('board');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database("./express-app.db");
+router.get('/',(_, res) => {
+	var sql = 'select * from content'
+	db.all(sql,(err,row) => {
+		console.log(row);
+		res.render('board',{ contents: row });
+	})
+	
 });
 
 module.exports = router;
